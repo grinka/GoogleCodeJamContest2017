abb9c66b34f19e7948d4362d2004074849b8c50d
Alexey Zelenin <grinka@gmail.com>
2017-04-18 19:26:03 -0400

    Add projects. AlphabetCake is complete, others - just initiated

---
+++ AlphabetCake/Program.cs
@@ -0,0 +1,97 @@
+﻿using System;
+
+namespace AlphabetCake {
+	class Program {
+		static void Main(string[] args) {
+			var tCount = int.Parse(Console.In.ReadLine());
+			for(var tIdx = 1; tIdx <= tCount; tIdx++) {
+				ProcessOneTest(tIdx);
+			}
+		}
+
+		static void ProcessOneTest(int testIdx) {
+			var testLine = Console.In.ReadLine().Split(' ');
+			var rowsCount = int.Parse(testLine[0]);
+			var colsCount = int.Parse(testLine[1]);
+			var chars = new char[colsCount, rowsCount];
+			var gen = new char[colsCount, rowsCount];
+			for(var rowIdx = 0; rowIdx < rowsCount; rowIdx++) {
+				for(var colIdx = 0; colIdx < colsCount; colIdx++) {
+					chars[colIdx, rowIdx] = (char)Console.In.Read();
+					gen[colIdx, rowIdx] = chars[colIdx, rowIdx];
+				}
+				Console.In.ReadLine();
+			}
+
+			for(var r = 0; r < rowsCount; r++) {
+				for(var c = 0; c < colsCount; c++) {
+					if(chars[c, r] != '?') {
+						var s = chars[c, r];
+						// need to populate
+						var c1 = c;
+						var c2 = c;
+						var r1 = r;
+						var r2 = r;
+						while(FloodH(gen, r1, r2, c1 - 1, s)) {
+							c1--;
+						}
+						while(FloodV(gen, c1, c2, r1 - 1, s)) {
+							r1--;
+						}
+						while(FloodH(gen, r1, r2, c2 + 1, s)) {
+							c2++;
+						}
+						while(FloodV(gen, c1, c2, r2 + 1, s)) {
+							r2++;
+						}
+					}
+				}
+			}
+			Console.Out.WriteLine($"Case #{testIdx}:");
+			for(var row = 0; row < rowsCount; row++) {
+				for(var col = 0; col < colsCount; col++) {
+					Console.Out.Write(gen[col, row]);
+				}
+				Console.Out.WriteLine();
+			}
+		}
+
+		static bool FloodH(char[,] chars, int r1, int r2, int newcol, char s) {
+			if(newcol<0 || newcol>=chars.GetLength(0)) {
+				return false;
+			}
+			var canMove = true;
+			for(var ri = r1; ri <= r2; ri++) {
+				if(chars[newcol, ri] != '?') {
+					canMove = false;
+				}
+			}
+			if(canMove) {
+				for(var ri = r1; ri <= r2; ri++) {
+					chars[newcol, ri] = s;
+				}
+				return true;
+			}
+			return false;
+		}
+
+		static bool FloodV(char[,] chars, int c1, int c2, int newrow, char s) {
+			if(newrow <0 || newrow >= chars.GetLength(1)) {
+				return false;
+			}
+			var canMove = true;
+			for(var ci = c1; ci <= c2; ci++) {
+				if(chars[ci, newrow] != '?') {
+					canMove = false;
+				}
+			}
+			if(canMove) {
+				for(var ci = c1; ci <= c2; ci++) {
+					chars[ci, newrow] = s;
+				}
+				return true;
+			}
+			return false;
+		}
+	}
+}
---
+++ Ratatouille/Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ratatouille {
+	class Program {
+
+		private class Package {
+			public int Size { get; set; }
+			public double MinP { get; set; }
+			public double MaxP { get; set; }
+
+			public Package(int size, int n) {
+				Size = size;
+				MinP = Math.Ceiling((size / 1.1) / n);
+				MaxP = Math.Floor((size / 0.9) / n);
+			}
+		}
+
+		private class Supply {
+			public Supply() {
+				Packages = new Stack<Package>();
+			}
+			public Stack<Package> Packages { get; set; }
+		}
+
+		static void Main(string[] args) {
+			var tCount = int.Parse(Console.In.ReadLine());
+			for(var tIdx = 1; tIdx <= tCount; tIdx++) {
+				ProcessOneTest(tIdx);
+			}
+			Console.WriteLine();
+			Console.WriteLine("Press ENTER to exit.");
+			Console.ReadLine();
+		}
+
+		static void ProcessOneTest(int testIdx) {
+			var tLine = Console.In.ReadLine().Split(' ');
+			var N = int.Parse(tLine[0]); // number of ingridients
+			var P = int.Parse(tLine[1]); // number of packages
+			var nValuesLine = Console.In.ReadLine().Split(' ');
+			var nValues = nValuesLine.Select(x => int.Parse(x)).ToArray();
+			var pValues = new Supply[N];
+			var foundMatches = 0;
+			for(var i = 0; i < N; i++) {
+				var pValuesLine = Console.In.ReadLine().Split(' ');
+				pValues[i] = new Supply();
+				var plist = new List<Package>();
+				for(var j = 0; j < P; j++) {
+					var package = new Package(int.Parse(pValuesLine[j]), nValues[i]);
+					plist.Add(package);
+				}
+				foreach(var p in plist.OrderByDescending(x => x.MinP)) {
+					pValues[i].Packages.Push(p);
+				}
+			}
+
+			if(N == 1) {
+				Console.WriteLine("1");
+			} else {
+				while(pValues[0].Packages.Any() && pValues[1].Packages.Any()) {
+					while(pValues[0].Packages.Any() && pValues[0].Packages.Peek().MaxP < pValues[1].Packages.Peek().MinP) {
+						pValues[0].Packages.Pop();
+					}
+					if(pValues[0].Packages.Any()) {
+						while(pValues[1].Packages.Any() && pValues[1].Packages.Peek().MaxP < pValues[0].Packages.Peek().MinP) {
+							pValues[1].Packages.Pop();
+						}
+
+						if(pValues[1].Packages.Any() && InSame(pValues[1].Packages.Peek(), pValues[0].Packages.Peek())) {
+							pValues[1].Packages.Pop();
+							pValues[0].Packages.Pop();
+							foundMatches++;
+						}
+					}
+				}
+			}
+
+			Console.Out.WriteLine($"Case #{testIdx}: {foundMatches}");
+
+		}
+
+		static bool InSame(Package x, Package y) {
+			return x.MinP <= y.MaxP && y.MinP <= x.MaxP;
+		}
+	}
+}
