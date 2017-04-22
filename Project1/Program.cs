f263f2cddae8c0ee6232f63f361ff8197d781676
Alexey Zelenin <grinka@gmail.com>
2017-04-22 13:42:08 -0400

    Add Project files for 1B round. 1st task is solved. Second - does not work on dataset

---
+++ Project1/Program.cs
@@ -0,0 +1,43 @@
+﻿/// <summary>
+/// Google CodeJam Contest.
+/// Round 1B: Problem A. Steed 2: Cruise Control
+/// </summary>
+namespace Project1 {
+	using System;
+	using System.Collections.Generic;
+	using System.Linq;
+	using System.Text;
+	using System.Threading.Tasks;
+	class Program {
+		static void Main(string[] args) {
+			var testQty = int.Parse(Console.In.ReadLine());
+			for(var t = 1; t <= testQty; t++) {
+				ProcessOneTest(t);
+			}
+			//DisplayEnter();
+		}
+
+		static void DisplayEnter() {
+			Console.WriteLine();
+			Console.WriteLine("Press ENTER to exit.");
+			Console.ReadLine();
+		}
+
+		static void ProcessOneTest(int testIdx) {
+			var l = Console.In.ReadLine().Split(' ');
+			var D = int.Parse(l[0]);
+			var N = int.Parse(l[1]);
+			float maxTime = 0.0f;
+			for(var n = 0; n < N; n++) {
+				l = Console.In.ReadLine().Split(' ');
+				var k = int.Parse(l[0]);
+				var s = int.Parse(l[1]);
+				float newTime = ((float)(D - k)) / s;
+				if(newTime > maxTime)
+					maxTime = newTime;
+			}
+			float cruiseSpeed = D / maxTime;
+			Console.Out.WriteLine($"Case #{testIdx}: {cruiseSpeed:#.0########}");
+		}
+	}
+}
---
+++ Project3/Program.cs
@@ -0,0 +1,24 @@
+﻿/// <summary>
+/// Google CodeJam Contest.
+/// Round 1B: Problem C.
+/// </summary>
+namespace Project3 {
+	using System;
+	using System.Collections.Generic;
+	using System.Linq;
+	using System.Text;
+	using System.Threading.Tasks;
+
+	class Program {
+		static void Main(string[] args) {
+			var testQty = int.Parse(Console.In.ReadLine());
+			for(var t = 1; t <= testQty; t++) {
+				ProcessOneTest(t);
+			}
+		}
+
+		static void ProcessOneTest(int testIdx) {
+
+		}
+	}
+}
