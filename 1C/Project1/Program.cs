c732cc79b5e46931c20fb700a17e8e14252b4c87
Alexey Zelenin <grinka@gmail.com>
2017-04-30 21:26:59 -0400

    Task A for 1C works with small dataset now For some reasons large dataset is incorrect

--- 1C/Project1/Program.cs
+++ 1C/Project1/Program.cs
@@ -1,14 +1,12 @@
 ﻿/// <summary>
 /// Google CodeJam Contest.
-/// Round 1C: Problem A
+/// Round 1C: Problem A: Ample Syrup
 /// </summary>
 namespace GoogleCodeJam.Contest2017.Round1C.Project1 {
 
 	using System;
 	using System.Collections.Generic;
 	using System.Linq;
-	using System.Text;
-	using System.Threading.Tasks;
 
 	class Program1 {
 		static void Main(string[] args) {
@@ -26,6 +24,46 @@ namespace GoogleCodeJam.Contest2017.Round1C.Project1 {
 		}
 
 		static void ProcessOneTest(int testIdx) {
+			var l = Console.In.ReadLine().Split(' ');
+			var N = int.Parse(l[0]);
+			var K = int.Parse(l[1]);
+			var stack = new List<Cake>();
+			for(var i = 0; i < N; i++) {
+				l = Console.In.ReadLine().Split(' ');
+				var c = new Cake(ulong.Parse(l[0]), ulong.Parse(l[1]), i);
+				stack.Add(c);
+			}
+
+			decimal sum = 0.0M;
+			// find K max items
+			var widest = stack.OrderByDescending(x => x.AddedSize).Take(K).Max(x => x.r);
+			var baseC = stack.Where(x => x.r >= widest).OrderByDescending(x => x.CoveredSize).First();
+			var addedAizesList = stack
+				.Where(x => (x.idx != baseC.idx && x.r <= baseC.r))
+				.OrderByDescending(x => x.AddedSize)
+				.Take(K - 1);
+			sum = baseC.CoveredSize;
+			foreach(var ac in addedAizesList) {
+				sum += ac.AddedSize;
+			}
+			var result = (decimal)(sum) * (decimal)Math.PI;
+
+			Console.Out.WriteLine($"Case #{testIdx}: {result:#.0##########}");
+		}
+
+		private class Cake {
+			public ulong r { get; }
+			public ulong h { get; }
+			public int idx { get; }
+			public Cake(ulong r, ulong h, int idx) {
+				this.r = r;
+				this.h = h;
+				this.idx = idx;
+				this.CoveredSize = r * (r + 2 * h);
+				this.AddedSize = 2 * r * h;
+			}
+			public ulong CoveredSize { get; }
+			public ulong AddedSize { get; }
 		}
 	}
 }
